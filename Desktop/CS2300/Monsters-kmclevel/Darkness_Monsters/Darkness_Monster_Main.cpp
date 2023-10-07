6dfdf48bd767b4954bc82765f0a6a97ae733f686
Kane Cleveland <kmclev21@stlawu.edu>
2023-10-07 14:21:54 -0400

    repo cloned

---
+++ Desktop/CS2300/Monsters-kmclevel/Darkness_Monsters/Darkness_Monster_Main.cpp
@@ -0,0 +1,21 @@
+#include "Closet_Monster.h"
+#include "Under_Bed_Monster.h"
+#include <ctime>
+#include <iostream>
+#include <memory>
+#include <vector>
+using namespace std;
+
+int main() {
+    /*
+     * TODO: Create a vector of Darkness Monster unique pointers and push a variety of ten Closet Monsters and Under Bed Monsters to it.
+     * TODO: Tell the user that there are monster eyes staring at them from the darkness!
+     * TODO: Repeatedly give the user the option to poke the eyes or run away.
+     *     - If they choose to poke the eyes, loop through the vector and call the poke_eye and scare method on each monster. Output the scare method return values to the console.
+     *         - Note: the calls to poke_eye are NOT polymorphism.
+     *         - Note: the calls to scare are polymorphism.
+     *     - If they choose to run away, the program ends.
+     */
+    return 0;
+}
+
